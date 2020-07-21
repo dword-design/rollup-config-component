d9b61b15a90af65e87f07bcce2fb70436931cff5
foo <foo@bar.de>
2020-07-21 02:17:00 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,42 @@
+<!-- TITLE/ -->
+# rollup-config-component
+<!-- /TITLE -->
+
+<!-- BADGES/ -->
+[![NPM version](https://img.shields.io/npm/v/rollup-config-component.svg)](https://npmjs.org/package/rollup-config-component)
+![Linux macOS Windows compatible](https://img.shields.io/badge/os-linux%20%7C%C2%A0macos%20%7C%C2%A0windows-blue)
+[![Build status](https://img.shields.io/github/workflow/status/dword-design/rollup-config-component/build)](https://github.com/dword-design/rollup-config-component/actions)
+[![Coverage status](https://img.shields.io/coveralls/dword-design/rollup-config-component)](https://coveralls.io/github/dword-design/rollup-config-component)
+[![Dependency status](https://img.shields.io/david/dword-design/rollup-config-component)](https://david-dm.org/dword-design/rollup-config-component)
+![Renovate enabled](https://img.shields.io/badge/renovate-enabled-brightgreen)
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/dword-design/rollup-config-component)
+<!-- /BADGES -->
+
+<!-- DESCRIPTION/ -->
+
+<!-- /DESCRIPTION -->
+
+<!-- INSTALL/ -->
+## Install
+
+```bash
+# NPM
+$ npm install rollup-config-component
+
+# Yarn
+$ yarn add rollup-config-component
+```
+<!-- /INSTALL -->
+
+<!-- LICENSE/ -->
+## License
+
+Unless stated otherwise all works are:
+
+Copyright &copy; Sebastian Landwehr <info@dword-design.de>
+
+and licensed under:
+
+[MIT License](https://opensource.org/licenses/MIT)
+<!-- /LICENSE -->
---
+++ src/index.js
@@ -0,0 +1,149 @@
+import { compact, join } from '@dword-design/functions'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import fs from 'fs-extra'
+import loadPkg from 'load-pkg'
+import minimist from 'minimist'
+import parsePkgName from 'parse-pkg-name'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import vue from 'rollup-plugin-vue'
+
+const packageConfig = loadPkg.sync()
+const nameParts = parsePkgName(packageConfig.name)
+const stylePrefix = [nameParts.org, nameParts.name] |> compact |> join('-')
+// Get browserslist config and remove ie from es build targets
+const esbrowserslist = fs
+  .readFileSync('./.browserslistrc')
+  .toString()
+  .split('\n')
+  .filter(entry => entry && entry.substring(0, 2) !== 'ie')
+const argv = minimist(process.argv.slice(2))
+const baseConfig = {
+  input: 'src/entry.js',
+  plugins: {
+    babel: {
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+    },
+    replace: {
+      'process.env.ES_BUILD': JSON.stringify('false'),
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    },
+    vue: {
+      css: true,
+      style: {
+        postcssModulesOptions: {
+          generateScopedName: `${stylePrefix}__[local]__[hash:base64:4]`,
+          hashPrefix: stylePrefix,
+        },
+      },
+      template: {
+        isProduction: true,
+      },
+    },
+  },
+}
+// ESM/UMD/IIFE shared settings: externals
+// Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
+const external = [
+  // list external dependencies, exactly the way it is written in the import statement.
+  // eg. 'jquery'
+  'vue',
+]
+// UMD/IIFE shared settings: output.globals
+// Refer to https://rollupjs.org/guide/en#output-globals for details
+const globals = {
+  // Provide global variable names to replace your external imports
+  // eg. jquery: '$'
+  vue: 'Vue',
+}
+// Customize configs for individual targets
+const buildFormats = []
+if (!argv.format || argv.format === 'es') {
+  const esConfig = {
+    ...baseConfig,
+    external,
+    output: {
+      exports: 'named',
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+    plugins: [
+      replace({
+        ...baseConfig.plugins.replace,
+        'process.env.ES_BUILD': JSON.stringify('true'),
+      }),
+      vue(baseConfig.plugins.vue),
+      babel({
+        ...baseConfig.plugins.babel,
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: esbrowserslist,
+            },
+          ],
+        ],
+      }),
+      commonjs(),
+    ],
+  }
+  buildFormats.push(esConfig)
+}
+if (!argv.format || argv.format === 'cjs') {
+  const umdConfig = {
+    ...baseConfig,
+    external,
+    output: {
+      compact: true,
+      exports: 'named',
+      file: 'dist/index.ssr.js',
+      format: 'cjs',
+      globals,
+      name: 'VueIcon',
+    },
+    plugins: [
+      replace(baseConfig.plugins.replace),
+      vue({
+        ...baseConfig.plugins.vue,
+        template: {
+          ...baseConfig.plugins.vue.template,
+          optimizeSSR: true,
+        },
+      }),
+      babel(baseConfig.plugins.babel),
+      commonjs(),
+    ],
+  }
+  buildFormats.push(umdConfig)
+}
+if (!argv.format || argv.format === 'iife') {
+  const unpkgConfig = {
+    ...baseConfig,
+    external,
+    output: {
+      compact: true,
+      exports: 'named',
+      file: 'dist/index.min.js',
+      format: 'iife',
+      globals,
+      name: 'VueIcon',
+    },
+    plugins: [
+      replace(baseConfig.plugins.replace),
+      vue(baseConfig.plugins.vue),
+      babel(baseConfig.plugins.babel),
+      commonjs(),
+      terser({
+        output: {
+          ecma: 5,
+        },
+      }),
+    ],
+  }
+  buildFormats.push(unpkgConfig)
+}
+
+// Export config
+export default buildFormats
